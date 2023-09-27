93e37daed8ec32e2f74b7f6d4d5c5f311025cf5f
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-09-27 21:55:34 +0900

    백업푸시
    백업푸시

---
+++ 프로그래머스 코테/탐욕법/3.큰 수 만들기.js
@@ -0,0 +1,14 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=javascript
+
+/*
+만들고자 하는 이름 name이 매개변수로 주어질 때, 
+이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
+*/
+
+
+
+/*
+다른 풀이
+
+*/
