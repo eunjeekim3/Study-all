03364707432e19e81bf4a5126d1cba50494e95b1
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-05 22:39:48 +0900

    정렬 - H_Index
    작성중...

---
+++ 프로그래머스 코테/정렬/3.H_Index.js
@@ -0,0 +1,13 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=javascript
+
+/*
+어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 
+이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(citations) {
+    var answer = 0;
+    return answer;
+}
