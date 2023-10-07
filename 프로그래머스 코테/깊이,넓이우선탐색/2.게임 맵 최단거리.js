ec2d13a8a4a293d4996d03c4c77c56627f94976e
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-07 14:26:00 +0900

    깊이,넓이우선탐색 - 게임 맵 최단거리
    작성중...

---
+++ 프로그래머스 코테/깊이,넓이우선탐색/2.게임 맵 최단거리.js
@@ -0,0 +1,13 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript
+
+/*
+게임 맵의 상태 maps가 매개변수로 주어질 때, 캐릭터가 상대 팀 진영에 
+도착하기 위해서 지나가야 하는 칸의 개수의 최솟값을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 작성중...
+function solution(maps) {
+    var answer = 0;
+    return answer;
+}
