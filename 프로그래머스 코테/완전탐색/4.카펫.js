0b27e277d20f896130a11c6cf003dc49aa6af8fc
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-08 20:45:58 +0900

    완전탐색 - 카펫
    작성중...

---
+++ 프로그래머스 코테/완전탐색/4.카펫.js
@@ -0,0 +1,13 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=javascript
+
+/*
+Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yellow가 매개변수로 주어질 때 카펫의 가로, 세로 크기를 
+순서대로 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(brown, yellow) {
+    var answer = [];
+    return answer;
+}
