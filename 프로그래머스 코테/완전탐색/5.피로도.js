2ff17eece24fd20c839a823af0a4824155080562
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-10 22:36:55 +0900

    완전탐색 - 피로도
    작성중...

---
+++ 프로그래머스 코테/완전탐색/5.피로도.js
@@ -0,0 +1,13 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript
+
+/*
+유저의 현재 피로도 k와 각 던전별 "최소 필요 피로도", "소모 피로도"가 담긴 2차원 배열 dungeons 가 매개변수로 주어질 때, 유저가 탐험할수 있는 
+최대 던전 수를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 작성ing
+function solution(k, dungeons) {
+    var answer = -1;
+    return answer;
+}
