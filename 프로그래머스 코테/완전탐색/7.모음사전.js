0cac5835dbc3c4cccab67b7c2d23670861acf129
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-13 23:21:55 +0900

    완전탐색 - 모음사전
    작성ing...

---
+++ 프로그래머스 코테/완전탐색/7.모음사전.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript
+
+/*
+단어 하나 word가 매개변수로 주어질 때, 이 단어가 사전에서 
+몇 번째 단어인지 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 작성ing
+function solution(word) {
+  var answer = 0;
+
+  
+  return answer;
+}
