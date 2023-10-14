f04ed68bcc670a64cf3751273b0682ce19cd5e3e
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-14 23:21:11 +0900

    프로그래머스 문제풀이 64일차
    프로그래머스 레벨2
    - 완전탐색 : 모음사전

--- 프로그래머스 코테/완전탐색/7.모음사전.js
+++ 프로그래머스 코테/완전탐색/7.모음사전.js
@@ -6,10 +6,20 @@
 몇 번째 단어인지 return 하도록 solution 함수를 완성해주세요.
 */
 
-// 작성ing
-function solution(word) {
-  var answer = 0;
+// 풀이 해석
+let idx = 0; // 인덱스 초기화 변수
+const result = {}; // 인덱스 저장
+const vowels = [..."AEIOU"]; // 문자열을 배열로 변환
 
-  
-  return answer;
+function solution(word) { // 단어의 인덱스를 찾기 위한 함수
+  dfs("", 0); // 함수 호출해서 단어 생성 시작
+  return result[word]; // 함수의 인덱스 반환
 }
+
+const dfs = (word, length) => {
+  if (length > 5) return; // 길이가 5보다 크면 재귀 종료
+  result[word] = idx++; // 현재 단어에 인덱스 할당
+  vowels.forEach((vowel) => {
+    dfs(word + vowel, length + 1); // 각 모음을 덧붙여 재귀 호출
+  });
+};
