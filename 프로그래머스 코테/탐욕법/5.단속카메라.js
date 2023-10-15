468834be0bdf3e23cf9db48cb2ed69ddb1adb6ac
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-15 21:43:15 +0900

    탐욕법 - 단속카메라
    작성ing...

---
+++ 프로그래머스 코테/탐욕법/5.단속카메라.js
@@ -0,0 +1,15 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=javascript
+
+/*
+고속도로를 이동하는 차량의 경로 routes가 매개변수로 주어질 때, 
+모든 차량이 한 번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치해야 하는지를 return 하도록 solution 함수를 완성하세요.
+*/
+
+// 작성ing
+function solution(routes) {
+  var answer = 0;
+
+
+  return answer;
+}
