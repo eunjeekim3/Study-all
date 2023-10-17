7f0da936d30e8ab0d9c1fa5579045032b178cf72
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-17 22:14:39 +0900

    해시 - 베스트앨범
    작성ing...

---
+++ 프로그래머스 코테/해시/5.베스트앨범.js
@@ -0,0 +1,14 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=javascript
+
+/*
+베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 
+return 하도록 solution 함수를 완성하세요.
+*/
+
+function solution(genres, plays) {
+    var answer = [];
+    
+    
+    return answer;
+}
