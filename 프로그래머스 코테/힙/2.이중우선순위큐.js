b0423c615982fa2c1d2ce92549309117d1f00bc3
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-18 22:07:53 +0900

    힙 - 이중우선순위
    작성ing...

---
+++ 프로그래머스 코테/힙/2.이중우선순위큐.js
@@ -0,0 +1,17 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=javascript
+
+/*
+모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 
+[최댓값, 최솟값]을 return 하도록 solution 함수를 구현해주세요.
+*/
+
+//작성중...
+function solution(operations) {
+  var answer = [];
+  
+  
+  return answer;
+}
+
+
