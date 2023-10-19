6030647d37deb5d78d728d3909961d3fdff93463
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-19 22:10:33 +0900

    완전탐색 -.전력망을 둘로 나누기
    작성ing...

---
+++ 프로그래머스 코테/완전탐색/8.전력망을 둘로 나누기.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=javascript
+
+/*
+두 전력망이 가지고 있는 송전탑 개수의 
+차이(절대값)를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 작성중
+function solution(n, wires) {
+  var answer = -1;
+  
+  
+  return answer;
+}
