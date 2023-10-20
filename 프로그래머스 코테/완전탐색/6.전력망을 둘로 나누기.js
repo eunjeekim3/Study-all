0699d36f87c060d342c18cf6e632d8a70592a325
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-20 22:23:23 +0900

    프로그래머스 문제풀이 66일차
    프로그래머스 레벨2
    - 완전탐색 : 전력망을 둘로 나누기

--- 프로그래머스 코테/완전탐색/6.전력망을 둘로 나누기.js
+++ 프로그래머스 코테/완전탐색/6.전력망을 둘로 나누기.js
@@ -6,11 +6,41 @@
 return 하도록 solution 함수를 완성해주세요.
 */
 
-// 작성ing
+// 풀이 해석
 function solution(n, wires) {
-  var answer = -1;
+  var answer = Number.MAX_SAFE_INTEGER;
+  //트리 만들기
+  let tree = Array.from(Array(n+1),()=>[])
+  wires.map((element)=>{ // tree초기화
+      let [a,b] = element;
 
+      tree[a].push(b);
+      tree[b].push(a);
+  })
+ 
+  function searchTree(root, exceptNum) { // exceptNum은 제외하고 탐색
+      let count =0;
+      let visit = [];
+      let queue = [root];
+      visit[root] = true;
+      while(queue.length){
+          let index = queue.pop();
+          tree[index].forEach((element)=>{
+              if(element !== exceptNum && visit[element]!==true){
+                  visit[element] = true;
+                  queue.push(element);
+              }
+          })
+          count++;
+      }
+      
+      return count;
+  }
 
-  
+  // wires 값에 만든 함수에 값을 넣어 최솟값을 찾음.
+  wires.forEach(element => {
+      let[a,b] = element;
+      answer = Math.min(answer, Math.abs(searchTree(a,b)-searchTree(b,a)))
+  });
   return answer;
 }
