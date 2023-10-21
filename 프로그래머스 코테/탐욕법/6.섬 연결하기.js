5c59dbc4496f6fdace7c85ef0d8aa0625ed9a2fa
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-21 23:20:38 +0900

    탐욕법 - 섬 연결하기
    작성ing...

---
+++ 프로그래머스 코테/탐욕법/6.섬 연결하기.js
@@ -0,0 +1,14 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=javascript
+
+/*
+최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 
+최소 비용을 return 하도록 solution을 완성하세요.*/
+
+// 작성ing
+function solution(n, costs) {
+  var answer = 0;
+  
+  
+  return answer;
+}
