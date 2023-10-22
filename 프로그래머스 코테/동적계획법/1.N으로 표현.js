668ef8eb648b6b68ec504cafe8bd5e9671e038b7
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-22 22:27:56 +0900

    동적계획법 - N으로 표현
    작성ing...

---
+++ 프로그래머스 코테/동적계획법/1.N으로 표현.js
@@ -0,0 +1,14 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=javascript
+
+/*
+N과 사칙연산만 사용해서 표현 할 수 있는 방법 중 
+N 사용횟수의 최솟값을 return 하도록 solution 함수를 작성하세요.*/
+
+// 작성중...
+function solution(N, number) {
+    var answer = 0;
+    
+    
+    return answer;
+}
