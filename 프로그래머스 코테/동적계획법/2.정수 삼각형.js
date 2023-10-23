750a29c538bb849f1185b14dc2fc5a5d14b63fc0
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-23 22:44:58 +0900

    동적계획법 - 정수 삼각형
    작성ing...

---
+++ 프로그래머스 코테/동적계획법/2.정수 삼각형.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/parts/12263
+
+/*
+삼각형의 정보가 담긴 배열 triangle이 매개변수로 주어질 때, 
+거쳐간 숫자의 최댓값을 return 하도록 solution 함수를 완성하세요.
+*/
+
+// 작성중...
+function solution(triangle) {
+    var answer = 0;
+    
+    
+    return answer;
+}
