214a29e1dd5adc9ebf841521b4da7f63b31a1fd9
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-25 22:09:33 +0900

    깊이,넓이우선탐색 - 네트워크
    작성ing...

---
+++ 프로그래머스 코테/깊이,넓이우선탐색/3.네트워크.js
@@ -0,0 +1,15 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/parts/12421
+
+/*
+컴퓨터의 개수 n, 연결에 대한 정보가 담긴 2차원 배열 computers가 매개변수로 주어질 때, 
+네트워크의 개수를 return 하도록 solution 함수를 작성하시오.
+*/
+
+// 작성ing
+function solution(n, computers) {
+    var answer = 0;
+    
+    
+    return answer;
+}
