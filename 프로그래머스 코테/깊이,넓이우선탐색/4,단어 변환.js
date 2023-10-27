b9b90c918f7637c8affb7741971055b946760ab0
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-27 22:44:32 +0900

    깊이,넓이우선탐색 - 단어 변환
    작성ing...

---
+++ 프로그래머스 코테/깊이,넓이우선탐색/4,단어 변환.js
@@ -0,0 +1,15 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=javascript
+
+/*
+ 최소 몇 단계의 과정을 거쳐 begin을 target으로 변환할 수 있는지 
+ return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성ing
+function solution(begin, target, words) {
+    var answer = 0;
+    
+    
+    return answer;
+}
