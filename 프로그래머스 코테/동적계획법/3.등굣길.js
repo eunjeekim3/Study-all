8fbcb0fc626fbd98b9f8c659dbbddab60c9028db
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-10-31 22:25:15 +0900

    동적계획법 - 등굣길
    작성ing...

---
+++ 프로그래머스 코테/동적계획법/3.등굣길.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=javascript
+
+/*
+오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수를 1,000,000,007로 나눈 
+나머지를 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(m, n, puddles) {
+    var answer = 0;
+    
+    
+    return answer;
+}
