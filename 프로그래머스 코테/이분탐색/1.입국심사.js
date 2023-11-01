7cd28cbd0ccb220f4f41ac346a6d9743d01f05a5
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-01 22:33:20 +0900

    이분탐색 - 입국심사
    작성ing...

---
+++ 프로그래머스 코테/이분탐색/1.입국심사.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=javascript
+
+/*
+모든 사람이 심사를 받는데 걸리는 시간의 최솟값을 
+return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(n, times) {
+    var answer = 0;
+    
+    
+    return answer;
+}
