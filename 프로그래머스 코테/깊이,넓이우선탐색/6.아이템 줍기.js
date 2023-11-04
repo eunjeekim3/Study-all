027e98e589a28fbcdcf7fb9dbbfca8bb6a04c796
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-04 22:55:42 +0900

    깊이,넓이우선탐색 - 아이템 줍기
    작성ing...

---
+++ 프로그래머스 코테/깊이,넓이우선탐색/6.아이템 줍기.js
@@ -0,0 +1,15 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=javascript
+
+/*
+아이템을 줍기 위해 이동해야 하는 가장 짧은 거리를 
+return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 작성ing
+function solution(rectangle, characterX, characterY, itemX, itemY) {
+    var answer = 0;
+    
+    
+    return answer;
+}
