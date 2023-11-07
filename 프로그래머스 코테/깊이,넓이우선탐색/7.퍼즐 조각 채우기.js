c4f101e0c016b5d852af3bfb881626a1c0c4302a
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-07 23:09:43 +0900

    깊이,넓이우선탐색 - 퍼즐 조각 채우기
    작성ing...

---
+++ 프로그래머스 코테/깊이,넓이우선탐색/7.퍼즐 조각 채우기.js
@@ -0,0 +1,15 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/84021?language=javascript
+
+/*
+규칙에 맞게 최대한 많은 퍼즐 조각을 채워 넣을 경우, 
+총 몇 칸을 채울 수 있는지 return 하도록 solution 함수를 완성해주세요.
+*/
+
+// 작성ing
+function solution(game_board, table) {
+    var answer = -1;
+    
+    
+    return answer;
+}
