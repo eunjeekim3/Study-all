655610533f4ae3f349a006df6e4d4e052049318e
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-11 22:27:42 +0900

    그래프 - 순위
    작성ing...

---
+++ 프로그래머스 코테/그래프/2.순위.js
@@ -0,0 +1,15 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=javascript
+
+/*
+선수의 수 n, 경기 결과를 담은 2차원 배열 results가 매개변수로 주어질 때 
+정확하게 순위를 매길 수 있는 선수의 수를 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(n, results) {
+    var answer = 0;
+    
+    
+    return answer;
+}
