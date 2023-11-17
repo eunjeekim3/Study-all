7905ca97acaa4c535f28ea75af04df30374bd0b0
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-17 19:02:29 +0900

    이분탐색 - 징검다리
    작성ing...

---
+++ 프로그래머스 코테/이분탐색/2.징검다리.js
@@ -0,0 +1,15 @@
+// 레벨 4
+// https://school.programmers.co.kr/learn/courses/30/lessons/43236?language=javascript
+
+/*
+바위를 n개 제거한 뒤 각 지점 사이의 거리의 최솟값 중에 
+가장 큰 값을 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(distance, rocks, n) {
+    var answer = 0;
+    
+    
+    return answer;
+}
