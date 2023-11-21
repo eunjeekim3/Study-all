c8fb888cc0263210be9db058950b8b3d65deb114
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-21 23:28:14 +0900

    힙 - 디스크 컨트롤러
    작성ing...

---
+++ 프로그래머스 코테/힙/3.디스크 컨트롤러.js
@@ -0,0 +1,16 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=javascript
+
+/*
+작업의 요청부터 종료까지 걸린 시간의 평균을 가장 줄이는 방법으로 처리하면 
+평균이 얼마가 되는지 return 하도록 solution 함수를 작성해주세요. (단, 소수점 이하의 수는 버립니다)
+*/
+
+//작성중...
+function solution(jobs) {
+  var answer = 0;
+  
+  
+  return answer;
+}
+
