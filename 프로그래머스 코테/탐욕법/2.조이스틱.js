25831b4c0dfaf15eabfe3207db78c4dbba0f8692
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-24 22:16:36 +0900

    프로그래머스 문제풀이 72일차
    프로그래머스 레벨2
    - 탐욕법 : 조이스틱

--- 프로그래머스 코테/탐욕법/2.조이스틱.js
+++ 프로그래머스 코테/탐욕법/2.조이스틱.js
@@ -6,9 +6,28 @@
 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
 */
 
-
-
-/*
-다른 풀이
-
-*/
+// 풀이
+function solution(name) {
+    let answer = 0;
+    let min_move = name.length - 1;
+  
+    [...name].map((n, i) => {
+      // 현재 문자를 'A'로 변경하는 데 필요한 최소 이동 횟수를 계산
+      answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+      let idx = i + 1;
+  
+      // 연속되는 A의 개수 count
+      while (idx < name.length && name[idx] === 'A') {
+        idx++;
+      }
+  
+      // 필요한 최소 이동 횟수를 업데이트
+      min_move = Math.min(
+        min_move,
+        i * 2 + name.length - idx,
+        i + 2 * (name.length - idx),
+      );
+    });
+  
+    return answer + min_move;
+  }
