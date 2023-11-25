5f598bc61b6cb2e0e480fcc453f050b9f67c44b3
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-25 23:09:27 +0900

    PCCP기출문제 - 1번
    작성ing...

---
+++ 프로그래머스 코테/PCCP 기출문제/1.1번.js
@@ -0,0 +1,14 @@
+// 레벨 3
+// https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=javascript
+
+/*
+1번 노드로부터 가장 멀리 떨어진 노드가 몇 개인지를 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 작성중...
+function solution(n, edge) {
+    var answer = 0;
+    
+    
+    return answer;
+}
--- 프로그래머스 코테/탐욕법/2.조이스틱.js
+++ 프로그래머스 코테/탐욕법/2.조이스틱.js
@@ -1,33 +1,14 @@
-// 레벨 2
-// https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=javascript
+// 난이도 1
+// https://school.programmers.co.kr/learn/courses/30/lessons/250137
 
 /*
-만들고자 하는 이름 name이 매개변수로 주어질 때, 
-이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
+만약 몬스터의 공격을 받고 캐릭터의 체력이 0 이하가 되어 죽는다면 -1을 return 해주세요.
 */
 
-// 풀이
-function solution(name) {
-    let answer = 0;
-    let min_move = name.length - 1;
+// 작성ing...
+function solution(bandage, health, attacks) {
+  var answer = 0;
   
-    [...name].map((n, i) => {
-      // 현재 문자를 'A'로 변경하는 데 필요한 최소 이동 횟수를 계산
-      answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
-      let idx = i + 1;
   
-      // 연속되는 A의 개수 count
-      while (idx < name.length && name[idx] === 'A') {
-        idx++;
-      }
-  
-      // 필요한 최소 이동 횟수를 업데이트
-      min_move = Math.min(
-        min_move,
-        i * 2 + name.length - idx,
-        i + 2 * (name.length - idx),
-      );
-    });
-  
-    return answer + min_move;
-  }
+  return answer;
+}
