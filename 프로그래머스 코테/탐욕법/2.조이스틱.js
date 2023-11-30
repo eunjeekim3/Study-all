3cd05e1bc2db8601e6ac637039bfa15a77e2b4fc
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-11-30 22:37:01 +0900

    데이터 백업
    데이터 백업

--- 프로그래머스 코테/탐욕법/2.조이스틱.js
+++ 프로그래머스 코테/탐욕법/2.조이스틱.js
@@ -5,10 +5,26 @@
 만약 몬스터의 공격을 받고 캐릭터의 체력이 0 이하가 되어 죽는다면 -1을 return 해주세요.
 */
 
-// 작성ing...
-function solution(bandage, health, attacks) {
-  var answer = 0;
-  
-  
-  return answer;
+// 풀이
+function solution(name) {
+  let answer = 0;
+  let min_move = name.length - 1;
+
+  [...name].map((n, i) => {
+    answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+    let idx = i + 1;
+
+    // 연속되는 A의 개수 count
+    while (idx < name.length && name[idx] === 'A') {
+      idx++;
+    }
+
+    min_move = Math.min(
+      min_move,
+      i * 2 + name.length - idx,
+      i + 2 * (name.length - idx),
+    );
+  });
+
+  return answer + min_move;
 }
