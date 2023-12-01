8850a858cc13567dd079b3f74feb88cad1a223c1
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-01 22:03:34 +0900

    연습문제 - 연속된 부분 수열의 합
    작성ing...

---
+++ 프로그래머스 코테/연습문제/1.연속된 부분 수열의 합.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/178870
+
+/*
+위 조건을 만족하는 부분 수열의 시작 인덱스와 마지막 인덱스를 
+배열에 담아 return 하는 solution 함수를 완성해주세요.
+*/
+
+//작성ing...
+function solution(sequence, k) {
+  var answer = [];
+  
+  
+  return answer;
+}
