165fcab84061c833bb908d6f3c09aff41d870b8c
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-06 18:49:06 +0900

    연습문제 - 요격문제
    작성ing...

---
+++ 프로그래머스 코테/연습문제/2.요격문제.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/181188
+
+/*
+모든 폭격 미사일을 요격하기 위해 
+필요한 요격 미사일 수의 최솟값을 return 하도록 solution 함수를 완성해 주세요.
+*/
+
+//작성ing...
+function solution(targets) {
+  var answer = 0;
+  
+  
+  return answer;
+}
