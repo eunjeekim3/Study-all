77699c47fe53980808c8ad9aec1ab592bac90e3a
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-07 23:22:31 +0900

    연습문제 - 두 원 사이의 정수 쌍
    작성ing...

---
+++ 프로그래머스 코테/연습문제/3.두 원 사이의 정수 쌍.js
@@ -0,0 +1,14 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/181187
+
+/*
+두 원 사이의 공간에 x좌표와 y좌표가 모두 정수인 점의 개수를 return하도록 solution 함수를 완성해주세요.
+*/
+
+//작성ing...
+function solution(r1, r2) {
+  var answer = 0;
+  
+  
+  return answer;
+}
