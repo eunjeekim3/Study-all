d311becc783a27e1bf8b29de668a76e58b315638
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-22 21:21:35 +0900

    연습문제 - 최댓값과 최솟값
    작성ing...

---
+++ 프로그래머스 코테/연습문제/4.최댓값과 최솟값.js
@@ -0,0 +1,15 @@
+// 레벨 2
+// hhttps://school.programmers.co.kr/learn/courses/30/lessons/12939
+
+/*
+str에 나타나는 숫자 중 최소값과 최대값을 찾아 
+이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+*/
+
+//작성ing...
+function solution(s) {
+  var answer = '';
+  
+  
+  return answer;
+}
