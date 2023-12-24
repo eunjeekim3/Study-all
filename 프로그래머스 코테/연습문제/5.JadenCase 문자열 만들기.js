0bc7ca769ee4816cf6bbec5ef8398acceca54d63
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-24 23:06:20 +0900

    연습문제 - JadenCase 문자열 만들기
    작성ing...

---
+++ 프로그래머스 코테/연습문제/5.JadenCase 문자열 만들기.js
@@ -0,0 +1,14 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
+
+/*
+문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+*/
+
+// 작성ing...
+function solution(s) {
+  var answer = '';
+  
+  
+  return answer;
+}
