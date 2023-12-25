64958bf1f2ce1bb078834ff43e8816dfee78f106
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-25 17:30:14 +0900

    프로그래머스 문제풀이 75일차
    프로그래머스 레벨2
    - 연습문제 : JadenCase 문자열 만들기

--- 프로그래머스 코테/연습문제/5.JadenCase 문자열 만들기.js
+++ 프로그래머스 코테/연습문제/5.JadenCase 문자열 만들기.js
@@ -5,10 +5,26 @@
 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
 */
 
-// 작성ing...
+// 풀이
 function solution(s) {
   var answer = '';
-  
-  
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0 || s[i-1] === " ") {
+      answer += s[i].toUpperCase();
+    } else {
+      answer += s[i].toLowerCase();
+    }
+  }
+  return answer;
+}
+
+// 다른 풀이
+function solution(s) {
+  var answer = '';
+ let result = s.split(' ');
+
+ let result2 = result.map(x=> x.charAt(0).toUpperCase() + x.slice(1).toLowerCase());
+
+ answer = result2.join(" ")
   return answer;
 }
