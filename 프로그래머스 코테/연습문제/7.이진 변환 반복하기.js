d404b9ee68f942de44b491e05b048997d34a4404
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-31 13:39:01 +0900

    프로그래머스 문제풀이 77일차
    프로그래머스 레벨2
    - 연습문제 : 이진 변환 반복하기

--- 프로그래머스 코테/연습문제/7.이진 변환 반복하기.js
+++ 프로그래머스 코테/연습문제/7.이진 변환 반복하기.js
@@ -6,10 +6,39 @@
 return 하도록 solution 함수를 완성해주세요.
 */
 
-// 작성ing...
+// 풀이
 function solution(s) {
-  var answer = [];
-  
-  
+  var answer = [0, 0];
+  while (s.length > 1)
+  {
+      var cnt = 0
+      for(var i in [...s]){
+          if (s[i] == "1"){
+              cnt++
+          } else {
+              answer[1]+=1
+          }
+      }
+      answer[0]+=1
+      s = cnt.toString(2);
+  }
   return answer;
 }
+
+/*
+다른 풀이
+function solution(s) {
+  let answer = [0, 0];
+  let sLen = 0;
+
+  while (s.length > 1) {
+    sLen = s.length;
+    s = s.split("0").join("");
+    answer[0]++;
+    answer[1] += (sLen - s.length);
+    s = s.length.toString(2);
+  }
+
+  return answer;
+}
+*/
