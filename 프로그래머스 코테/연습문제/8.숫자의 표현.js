fc70c5e728d66c9e66db726d089fc96378323515
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-01 20:15:33 +0900

    연습문제 - 숫자의 표현
    작성ing...

---
+++ 프로그래머스 코테/연습문제/8.숫자의 표현.js
@@ -0,0 +1,20 @@
+// 레벨 2
+// https://school.programmers.co.kr/learn/courses/30/lessons/12924
+
+/*
+자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 
+n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
+*/
+
+// 작성ing...
+function solution(n) {
+  var answer = 0;
+  
+  
+  return answer;
+}
+
+/*
+다른 풀이
+
+*/
