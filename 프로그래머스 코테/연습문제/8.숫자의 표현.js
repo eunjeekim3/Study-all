b5647ad1ca2d4089c5907dffd68bb80b4005080d
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-02 18:47:05 +0900

    프로그래머스 문제풀이 78일차
    프로그래머스 레벨2
    - 연습문제 : 숫자의 표현

--- 프로그래머스 코테/연습문제/8.숫자의 표현.js
+++ 프로그래머스 코테/연습문제/8.숫자의 표현.js
@@ -6,15 +6,35 @@
 n을 표현하는 방법의 수를 return하는 solution를 완성해주세요.
 */
 
-// 작성ing...
+// 풀이
 function solution(n) {
-  var answer = 0;
-  
-  
+  let answer = 0;
+  let now = 1, count = 1, plusNum = 0;
+
+  while(count <= n) {
+    plusNum = plusNum + now;
+    if(plusNum >= n) {
+      if(plusNum === n) answer++;
+      plusNum = 0;
+      count++;
+      now = count;
+    } else {
+      now++;
+    }
+  }
+
   return answer;
 }
 
 /*
 다른 풀이
-
+function expressions(num) {
+    var answer = 0;
+    for (var i = 1; i <= num; i++) {
+        if ((num % i == 0) && (i % 2 == 1)) {
+            answer++;
+        }
+    }
+    return answer;
+  }
 */
