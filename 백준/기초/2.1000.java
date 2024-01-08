d7a55c93d046a14412466b138242b8673018a6b2
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-08 22:14:46 +0900

    문제풀이 83일차
    백준 기초 (자바)
    - 2557 (단순 출력)
    - 1000 (A+B)

---
+++ 백준/기초/2.1000.java
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/1000
+// A + B
+
+import java.util.Scanner;
+// import 안하니까 cannot find a symbol 에러남
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A + B);
+
+    }
+}
