23b65db88f161c08b53ef9d434e62f56aa80587e
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-11 21:21:14 +0900

    문제풀이 84일차
    백준 기초 (자바)
    - 1008 (A/B)
    - 10998 (A-B)

---
+++ 릿코드/1.2557.java
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/source/71331917
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("Hello World!"); 
+	}
+}
---
+++ 백준/기초/5.1008.java
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/10998
+// A - B
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        sc.close(); // 굳이 스캐너 닫는 이유 : 메모리 누수 방지
+        System.out.print(a / b);
+
+    }
+}
