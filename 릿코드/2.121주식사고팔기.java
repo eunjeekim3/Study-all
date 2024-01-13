ccc5b29a51fc42c7fd1c68487b0e5e43573ae807
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-13 23:29:13 +0900

    릿코드 풀이중...
    릿코드 풀이중...

---
+++ 릿코드/2.121주식사고팔기.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 작성중
+/*
+ * 해당 날짜의 특정 주식 가격을 나타내는 prices배열 이 제공됩니다 .prices[i]ith
+특정 주식을 구매할 하루를 선택하고 해당 주식을 판매할 미래의 다른 날을 선택 하여 수익을 극대화하려고 합니다 .
+이 거래에서 얻을 수 있는 최대 이익을 반환합니다 . 이익을 얻을 수 없으면 를 반환하십시오 0.
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+    }
+}
