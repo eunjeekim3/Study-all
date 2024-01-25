9d3f91423258758628d1fa93a939e2b7874198a9
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-01-25 22:26:20 +0900

    토마토 작성중
    토마토 작성중

---
+++ 백준/알고리즘스터디/6.토마토.java
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/7576
+// 토마토문제 자체를 이해못함... 작성중...
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
