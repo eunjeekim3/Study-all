af3e474133cfae523108716c30f64a4904fd574c
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-03-03 21:39:30 +0900

    문제풀이 96일차
    프로그래머스
    - 부분 문자열인지 확인하기
    - 정수 부분

---
+++ 프로그래머스 코테(java)/프로그래머스0(31~60번)/36.정수 부분.java
@@ -0,0 +1,12 @@
+// 레벨 0
+// https://school.programmers.co.kr/learn/courses/30/lessons/181850
+
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+
+        answer = (int) Math.floor(flo);
+
+        return answer;
+    }
+}
