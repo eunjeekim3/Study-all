2233a8f51d2072b9579b8d008a8bab0d8272d29e
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-03-14 21:41:55 +0900

    문제풀이 100일차
    프로그래머스
    - 조건에 맞게 수열 변환하기 1
    - 접두사인지 확인하기

---
+++ 프로그래머스 코테(java)/프로그래머스0(31~60번)/45.접두사인지 확인하기.java
@@ -0,0 +1,12 @@
+// 레벨 0
+// https://school.programmers.co.kr/learn/courses/30/lessons/181906
+
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if (my_string.startsWith(is_prefix)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
