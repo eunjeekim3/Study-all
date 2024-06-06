eae88e48644a049c30f5c36bbfbd464fae6750f7
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-06-06 21:16:01 +0900

    문제풀이 112일차
    프로그래머스
    - 뒤에서 5등까찌
    - 숫자 비교하기

---
+++ 프로그래머스 코테(java)/프로그래머스0(61~90번)/71.숫자 비교하기.java
@@ -0,0 +1,18 @@
+// 레벨 0
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+class Solution {
+    public int solution(int num1, int num2) {
+
+        if (num1 == num2) {
+            int answer = 1;
+            return answer;
+        }
+
+        else {
+            int answer = -1;
+            return answer;
+        }
+
+    }
+}
