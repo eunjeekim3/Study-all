1ec4691ec09bfbe45815c8ce3f3200cd9f2e81dc
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-06-09 22:30:30 +0900

    문제풀이 114일차
    프로그래머스
    - 몫 구하기
    - 두 수의 곱

---
+++ 프로그래머스 코테(java)/프로그래머스0(61~90번)/74.두 수의 곱.java
@@ -0,0 +1,11 @@
+// 레벨 0
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+import java.util.Scanner;
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+
+        return answer;
+    }
+}
