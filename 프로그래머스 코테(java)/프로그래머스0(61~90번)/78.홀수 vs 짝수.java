95c655645677d444a6cd86585c10c22a623ff800
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-06-16 12:14:47 +0900

    문제풀이 116일차
    프로그래머스
    - 홀수 vs 짝수

---
+++ 프로그래머스 코테(java)/프로그래머스0(61~90번)/78.홀수 vs 짝수.java
@@ -0,0 +1,19 @@
+// 레벨 0
+// https://school.programmers.co.kr/learn/courses/30/lessons/181887
+
+class Solution {
+
+    public int solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                even += num_list[i];
+            } else {
+                odd += num_list[i];
+            }
+        }
+        return Math.max(even, odd);
+    }
+}
