dfbd344aa84b1b5077f1332f8bd6ef3e6ece78ce
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-07-04 08:19:43 +0900

    반복문

--- 백준/반복문/Main.java
+++ 백준/반복문/Main.java
@@ -1,5 +1,7 @@
 /*
-
+같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
  */
 
 import java.io.BufferedReader;
@@ -12,22 +14,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str," ");
-		int H = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		if(M < 45) {
-			H--;
-			M = 60 - (45 - M);
-			if(H < 0) {
-				H = 23;
-			}
-			System.out.println(H + " " + M);
-		}
-		else {
-			System.out.println(H + " " + (M - 45));
-		}
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+ 
+        
+ 
+        System.out.println(hour + " " + minute);
     }
 }
 
--- 백준/조건문/Main.java
+++ 백준/조건문/Main.java
@@ -1,22 +1,24 @@
-// (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
-// (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+/*
+훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
 
-import java.util.Scanner;
+ */
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-		String B = sc.next();
-
-        sc.close();
+ import java.io.BufferedReader;
+ import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+ import java.io.IOException;
  
-        // charAt : 문자열의 인덱스 위치에 있는 문자(Character) 값을 반환. 아스키코드값으로 반환함.
-        // -'0'을 해줌으로써 문자열로서의 숫자가 아닌, 우리가 보는 숫자의 값이라고 안내하기 위함
-		System.out.println(A * (B.charAt(2) - '0'));
-		System.out.println(A * (B.charAt(1) - '0'));
-		System.out.println(A * (B.charAt(0) - '0'));
-		System.out.println(A * Integer.parseInt(B));
-    }
-}
+ public class Main {
+ 
+     public static void main(String[] args) throws IOException {
+        Integer[] a = new Integer[]{1, 2, 3}; // int[] 대신 Integer[] 사용
+        Arrays.sort(a, (s1, s2) -> (s2.toString() + s1.toString()).compareTo(s1.toString() + s2.toString())); // 비교자 수정
 
+        
+        System.out.println(Arrays.toString(a));
+
+     }
+ }
+ 
+ 
