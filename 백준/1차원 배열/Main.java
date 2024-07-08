f0046ac6a4ffa8e95c0044d1813073b703791a99
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-07-09 07:52:13 +0900

    배열

--- 백준/1차원 배열/Main.java
+++ 백준/1차원 배열/Main.java
@@ -1,71 +1,37 @@
 /*
-N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+X대학 M교수님은 프로그래밍 수업을 맡고 있다. 교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+
+교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
  */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.io.IOException;
+import java.util.Scanner;
 
-// public class Main {
-
-//     public static void main(String[] args) throws IOException {
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-//         // 첫 번째 줄: 정수의 개수 n
-//         int n = Integer.parseInt(br.readLine());
-
-//         // 두 번째 줄: 공백으로 구분된 n개의 정수
-//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-//         int[] numbers = new int[n];
-//         for (int i = 0; i < n; i++) {
-//             numbers[i] = Integer.parseInt(st.nextToken());
-//         }
-
-//         int max = numbers[0];
-//         int min = numbers[0];
-
-//         for (int i = 1; i < n; i++) {
-//             if (numbers[i] > max) {
-//                 max = numbers[i];
-//             }
-//             if (numbers[i] < min) {
-//                 min = numbers[i];
-//             }
-//         }
-
-//         br.close();
-
-//         System.out.println(min + " " + max);
-//     }
-// }
 public class Main {
-	public static void main(String[] args) {
 
+    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int[] student = new int[31];
 		
-		int N = sc.nextInt(); // 배열 길이
-		
-		int M = sc.nextInt(); // 반복 횟수
-		
-		int[] arr = new int[N];
-		
-		for(int x = 0 ; x < M ; x++) { // 
-			int i = sc.nextInt();
-			int j = sc.nextInt();
-			int k = sc.nextInt();
-			
-			for(int y = i-1 ; y < j ; y++) { // 입력 받은 줄에서의 반복
-				arr[y] = k;
-			}
-			
+		for(int i=1; i<29; i++) {
+			int success = sc.nextInt();
+			student[success] = 1;
+		}
+		for(int i=1; i<student.length; i++) {
+			if(student[i]!=1)
+				System.out.println(i);
 		}
 		
-		for(int x = 0 ; x < N ; x++) {
-			System.out.print(arr[x] + " ");
-		}
 		
+		sc.close();
+
 	}
 }
+
  
