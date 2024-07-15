1a071456b2fa43264a7344686f5c0999ea8b0fe3
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-07-16 08:36:01 +0900

    Update Main.java

--- 백준/반복문/Main.java
+++ 백준/반복문/Main.java
@@ -1,24 +1,31 @@
 /*
-훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
-
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제(근데 우측정렬)
  */
 
- import java.io.BufferedReader;
- import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
- import java.io.IOException;
+import java.io.IOException;
  
  public class Main {
  
-     public static void main(String[] args) throws IOException {
-        Integer[] a = new Integer[]{1, 2, 3}; // int[] 대신 Integer[] 사용
-        Arrays.sort(a, (s1, s2) -> (s2.toString() + s1.toString()).compareTo(s1.toString() + s2.toString())); // 비교자 수정
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
 
-        
-        System.out.println(Arrays.toString(a));
+        br.close();
 
-     }
- }
+        for(int i=1; i<number; i++) {
+            for(int j=1; j<=number-i ; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
  
  
