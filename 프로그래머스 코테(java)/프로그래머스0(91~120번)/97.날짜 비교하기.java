afb169449380bd25ecc78b72b2df1cd498570343
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2024-07-21 21:17:39 +0900

    프로그래머스 문제풀이
    프로그래머스
    - 배열 만들기 5
    - 짜 비교하기

---
+++ 프로그래머스 코테(java)/프로그래머스0(91~120번)/97.날짜 비교하기.java
@@ -0,0 +1,20 @@
+// 레벨 0
+// https://school.programmers.co.kr/learn/courses/30/lessons/181838
+
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 0;
+
+        for(int i=0; i < date1.length; i++) {
+            if(date1[i] < date2[i]) {
+                answer = 1;
+                break;
+            }else if(date1[i] > date2[i]){
+                answer = 0;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
